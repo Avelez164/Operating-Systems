142ccdd076a237e0c2145d5090130bf43422e9d9
Antonio Velez <112028104+Avelez164@users.noreply.github.com>
2025-04-12 09:07:48 -0700

    Add files via upload

--- countdown.c
+++ countdown.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <semaphore.h>
 #include <unistd.h>
 
 #define NUM_PHILOSOPHERS 5
 
-// Default rounds of eating
-int loops = 5;
+int loops = 3;
 
-// Forks rep as semaphores
-sem_t forks[NUM_PHILOSOPHERS];
+enum
+{
+    THINKING,
+    HUNGRY,
+    EATING
+} state[NUM_PHILOSOPHERS];
+pthread_cond_t conds[NUM_PHILOSOPHERS];
+pthread_mutex_t state_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-// countdown: number of philosophers still running
 int global_countdown;
 pthread_mutex_t countdown_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t all_done = PTHREAD_COND_INITIALIZER;
@@ -23,11 +26,43 @@ void think(int id)
     usleep(100000);
 }
 
-void eat(int id, int rounds_left)
+void eat(int id, int left)
 {
-    // rounds_left shows how many rounds remain
-    printf("P#%d EATING (%d left).\n", id, rounds_left);
+    printf("P#%d EATING (%d left).\n", id, left);
     usleep(100000);
+}
+
+int left(int id) { return (id + NUM_PHILOSOPHERS - 1) % NUM_PHILOSOPHERS; }
+int right(int id) { return (id + 1) % NUM_PHILOSOPHERS; }
+
+void test(int id)
+{
+    if (state[id] == HUNGRY &&
+        state[left(id)] != EATING &&
+        state[right(id)] != EATING)
+    {
+        state[id] = EATING;
+        pthread_cond_signal(&conds[id]);
+    }
+}
+
+void pickup_forks(int id)
+{
+    pthread_mutex_lock(&state_mutex);
+    state[id] = HUNGRY;
+    test(id);
+    while (state[id] != EATING)
+        pthread_cond_wait(&conds[id], &state_mutex);
+    pthread_mutex_unlock(&state_mutex);
+}
+
+void putdown_forks(int id)
+{
+    pthread_mutex_lock(&state_mutex);
+    state[id] = THINKING;
+    test(left(id));
+    test(right(id));
+    pthread_mutex_unlock(&state_mutex);
 }
 
 void *philosopher(void *arg)
@@ -35,31 +70,17 @@ void *philosopher(void *arg)
     int id = *(int *)arg;
     free(arg);
 
-    // philosophers start with the full number of rounds (loops)
     for (int i = loops; i > 0; i--)
     {
         think(id);
-        // ordering: even IDs pick up left fork first, odd IDs pick up right fork first
-        if (id % 2 == 0)
-        {
-            sem_wait(&forks[id]);
-            sem_wait(&forks[(id + 1) % NUM_PHILOSOPHERS]);
-        }
-        else
-        {
-            sem_wait(&forks[(id + 1) % NUM_PHILOSOPHERS]);
-            sem_wait(&forks[id]);
-        }
+        pickup_forks(id);
         eat(id, i);
-        sem_post(&forks[id]);
-        sem_post(&forks[(id + 1) % NUM_PHILOSOPHERS]);
+        putdown_forks(id);
         printf("P#%d finished eating, %d rounds remaining.\n", id, i - 1);
     }
 
-    // philosopher -> done, update the global countdown
     pthread_mutex_lock(&countdown_mutex);
     global_countdown--;
-    // signal -> main thread if last philosopher finished
     if (global_countdown == 0)
         pthread_cond_signal(&all_done);
     pthread_mutex_unlock(&countdown_mutex);
@@ -73,21 +94,18 @@ int main(int argc, char *argv[])
     {
         loops = atoi(argv[1]);
         if (loops <= 0)
-            loops = 5;
+            loops = 3;
     }
-
-    // set -> global countdown to -> # of philosophers
-    global_countdown = NUM_PHILOSOPHERS;
 
     pthread_t threads[NUM_PHILOSOPHERS];
+    global_countdown = NUM_PHILOSOPHERS;
 
-    // initialize fork semaphores
     for (int i = 0; i < NUM_PHILOSOPHERS; i++)
     {
-        sem_init(&forks[i], 0, 1);
+        pthread_cond_init(&conds[i], NULL);
+        state[i] = THINKING;
     }
 
-    // Create philosopher threads
     for (int i = 0; i < NUM_PHILOSOPHERS; i++)
     {
         int *id = malloc(sizeof(int));
@@ -97,15 +115,13 @@ int main(int argc, char *argv[])
 
     pthread_mutex_lock(&countdown_mutex);
     while (global_countdown > 0)
-    {
         pthread_cond_wait(&all_done, &countdown_mutex);
-    }
     pthread_mutex_unlock(&countdown_mutex);
 
     for (int i = 0; i < NUM_PHILOSOPHERS; i++)
     {
         pthread_join(threads[i], NULL);
-        sem_destroy(&forks[i]);
+        pthread_cond_destroy(&conds[i]);
     }
 
     printf("All philosophers have finished eating their rounds!\n");
